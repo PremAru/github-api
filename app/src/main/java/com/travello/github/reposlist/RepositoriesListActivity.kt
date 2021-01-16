627b36b2cef15f2ec8cda01c600376678067613e
Prem Arumugam <premanand.aru@gmail.com>
2021-01-17 07:29:05 +1100

    Design repos list and adapter UI

--- app/src/main/java/com/travello/github/reposlist/RepositoriesListActivity.kt
+++ app/src/main/java/com/travello/github/reposlist/RepositoriesListActivity.kt
@@ -1,11 +1,12 @@
-package com.travello.github
+package com.travello.github.reposlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.travello.github.R
 
-class MainActivity : AppCompatActivity() {
+class RepositoriesListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_repos_list)
     }
 }
